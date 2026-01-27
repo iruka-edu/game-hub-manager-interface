262bba1837cc555c1b8386cbd98699250f2f0350
Your Name <son.nguyen16@sotatek.com>
2026-01-27 15:22:02 +0700

    build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,17 @@
-import { cookies } from 'next/headers';
-import { redirect } from 'next/navigation';
-import { verifySession } from '@/lib/session';
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+import { verifySession } from "@/lib/session";
 
 export default async function HomePage() {
   const cookieStore = await cookies();
-  const token = cookieStore.get('iruka_session')?.value;
-  
+  const token = cookieStore.get("iruka_session")?.value;
+
   if (token) {
     const session = verifySession(token);
     if (session) {
-      redirect('/console');
+      redirect("/console");
     }
   }
-  
-  redirect('/login');
+
+  redirect("/login");
 }
